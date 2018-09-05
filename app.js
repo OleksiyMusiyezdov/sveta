b93a5f0c27f19fdf54ebfbac5e208df8d71829f2
Oleksiy Musiyezdov <musiyezdov@gmail.com>
2018-09-05 20:41:42 +0300

    Web-page redesign and Insert to database testing

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const app = express();
 var database = require('./database');
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
 
 app.get('/', function(req, res) {
   console.log('GET-request is working.');
-  res.render('index');
+  var data = {email: 'your email', pass: 'your password'};
+  console.log(data);
+  res.render('index', {data: data});
+});
+
+app.post('/', urlencodedParser, (req, res) => {
+  if (!req.body) return res.sendStatus(400);
+  console.log(req.body);
+  res.render('index', {data: req.body});
 });
 
 app.listen(3000, '127.0.0.1', console.log('Web-server is working.'));
--- database.js
+++ database.js
@@ -6,5 +6,14 @@ console.log('Connection to file "database.js"');
 MongoClient.connect(url, {useNewUrlParser: true}, function(err, db) {
   if(err) throw err;
   console.log('Connection to database established');
+
+ //new syntax instead of: var collection = db.collection('sveta-test')!
+  var collection = db.db().collection('sveta-test');
+  var user = {firstName: "Ivan", lastName: "Ivanov", age: 30};
+  collection.insertOne(user, function(err, result) {
+    if(err) throw err;
+    console.log(result.ops);
+  });
+
   db.close();
 });
